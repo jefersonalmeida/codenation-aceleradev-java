22739c60efac2c49d4ff3830cb86d9bc9a3f6a05
Jeferson Almeida <jeferson.almeida@outlook.com>
2020-05-19 13:19:16 -0400

    Criando Entidades de Banco de Dados em Java

---
+++ java-9/src/main/java/com/challenge/entity/User.java
@@ -0,0 +1,56 @@
+package com.challenge.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @NotNull
+    @Size(max = 100)
+    private String fullName;
+
+    @Column
+    @Email
+    @NotNull
+    @Size(max = 100)
+    private String email;
+
+    @Column
+    @Size(max = 50)
+    @NotNull
+    private String nickname;
+
+    @Column
+    @Size(max = 255)
+    @NotNull
+    private String password;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "id.user")
+    private List<Candidate> candidates;
+
+    @OneToMany(mappedBy = "id.user")
+    private List<Submission> submissions;
+}
