4f666ce4fec14434c1eafae2a990eb042d624fd4
Jeferson Almeida <jeferson.almeida@outlook.com>
2020-05-26 10:16:46 -0400

    Desafio 5
    Gerador de frases do Monty Python utilizando Spring Boot e JPA

---
+++ java-5/src/main/java/challenge/QuoteRepository.java
@@ -0,0 +1,14 @@
+package challenge;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface QuoteRepository extends JpaRepository<Quote,Integer> {
+
+    List<Quote> findByActor(String actor);
+
+    @Query("select max(id) from Quote")
+    Integer findMaxId();
+}
---
+++ java-5/src/main/java/challenge/QuoteServiceImpl.java
@@ -0,0 +1,27 @@
+package challenge;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class QuoteServiceImpl implements QuoteService {
+
+    private final Random random = new Random();
+    @Autowired
+    private QuoteRepository repository;
+
+    @Override
+    public Quote getQuote() {
+        Integer max = repository.findMaxId();
+        return repository.findById(random.nextInt(max)).orElseThrow(RuntimeException::new);
+    }
+
+    @Override
+    public Quote getQuoteByActor(String actor) {
+        List<Quote> byActor = repository.findByActor(actor);
+        return byActor.get(random.nextInt(byActor.size()));
+    }
+}
